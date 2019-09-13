65ce1053c48d8842e6a5f013fc27c00c9386700f
Sergio Florez <sergioflorezp@gmail.com>
2019-09-13 18:24:09 -0500

    doc: first version of readme added

--- example.py
+++ example.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     decoded_data = huffman_decoding(encoded_data, tree)
 
     print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print("The content of the encoded data is: {}\n".format(decoded_data))
+    print("The content of the decoded data is: {}\n".format(decoded_data))
 
     print("The original and decoded data are the same?:", sentence == decoded_data)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Huffman coding
+
+This is a implementation of Huffman coding for the course Data Structures and Algorithms in udacity.
+
+Feel free to use for academic purposes.
+
+To know how to use this code you could refer to the file example.py, in the folder.
